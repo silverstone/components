d3e71d6a29913a2388d078da001a48113680468f
Royce <royceshayegan@gmail.com>
2018-10-07 09:09:54 -0400

    popover updates

--- src/components/ss-popover/ss-popover.tsx
+++ src/components/ss-popover/ss-popover.tsx
@@ -1,5 +1,4 @@
-import { Component, Prop, State, Method, Listen } from '@stencil/core'
-import { Backdrop } from '../ss-backdrop/ss-backdrop';
+import { Component, Prop, State, Method, Element } from '@stencil/core'
 
 @Component({
   tag: 'ss-popover',
@@ -8,44 +7,174 @@ import { Backdrop } from '../ss-backdrop/ss-backdrop';
 })
 export class Popover { 
 
-  @Prop() backdrop: boolean = false
-  @Prop() closeOutside: boolean = true
-  @Prop() backdropCloseButton: boolean = false
+	@Element() el: HTMLElement
+	popoverEl: HTMLElement
 
-  @State() show: boolean = false
+	outsideClickListener = () => this.close()
+	popoverClickListener = event => event.stopPropagation()
 
-  @Method()
-  open() {
-    this.show = true;
-  }
+	//Props
+	@Prop() type: string = "static"
+	@Prop() align: string = "center"
+	@Prop() position: string = "bottom"
+	@Prop() margin: number = 24
+	@Prop() offsetY: number = 0
+	@Prop() offsetX: number = 0
 
-  @Method()
-  @Listen('clickCloseButton')
-  close() {
-    this.show = false
-  }
+	//States
+	@State() positionX: number = 0
+	@State() positionY: number = 42
+	@State() show: boolean = false
+	
+	//Methods
+	@Method()
+		open() {
+			if (this.popoverEl.classList.contains("show")) {
+				this.show = false
+			} else {
+				this.show = false
+				setTimeout(() => {
+				this.show = true
+				window.addEventListener('click', this.outsideClickListener)
+				this.popoverEl.addEventListener('click', this.popoverClickListener)
+				}, 100)
+			}
+		}
+		
+	@Method()
+		close() {
+			this.show = false
+		}
 
-  render() {
-    return ([
-        <ss-backdrop 
-        show={this.show}
-        invisible={!this.backdrop}
-        close-button={this.backdropCloseButton}
-        onClick={() => this.closeOutside && this.close()}
-      >
-        <slot name="backdrop-close-button">
-          X
-        </slot>
-      </ss-backdrop>,
-        //Popover Toggle
-        <div class="has-popover">
-            <a href="#" class="popover-toggle"
-            onClick={() => this.open()}>Test</a>
-        </div>,
+	//Lifecycle Hooks	
+	componentDidLoad() {
+		
+		//Relative to container
+		var containerHeight = this.el.offsetHeight
+		var containerWidth = this.el.offsetWidth
 
-        //Popover
-        <div class={"popover" + (this.show ? " show" : "")}>
-        </div>
-    ])
-  }
+		var popoverHeight = this.popoverEl.offsetHeight
+		var popoverWidth = this.popoverEl.offsetWidth
+
+		var containerCenterY = this.el.offsetHeight / 2
+		var containerCenterX = this.el.offsetWidth / 2
+
+		var popoverCenterY = this.popoverEl.offsetHeight / 2
+		var popoverCenterX = this.popoverEl.offsetWidth / 2
+
+		var posTop = -popoverHeight + -this.margin + this.offsetY
+		var posLeft = -popoverWidth + -this.margin + this.offsetX
+		var posRight = containerWidth + this.margin + this.offsetX
+		var posBottom = containerHeight + this.margin + this.offsetY
+
+		var alignCenterY = containerCenterY - popoverCenterX + this.offsetY
+		var alignCenterX = containerCenterX - popoverCenterX + this.offsetX
+
+		var alignTop = 0 + this.offsetY
+		var alignBottom = containerHeight - popoverHeight + this.offsetY
+		var alignLeft = 0 + this.offsetX
+		var alignRight = containerWidth - popoverWidth + this.offsetX
+
+		//Bottom
+		if (this.align =="center" && this.position =="bottom") {
+			this.positionX = alignCenterX
+			this.positionY = posBottom
+		}
+		if (this.align =="right" && this.position =="bottom") {
+			this.positionX = alignRight
+			this.positionY = posBottom
+		}
+		if (this.align =="left" && this.position =="bottom") {
+			this.positionX = alignLeft
+			this.positionY = posBottom
+		}
+
+		//Right
+		if (this.align =="center" && this.position =="right") {
+			this.positionX = posRight
+			this.positionY = alignCenterY
+		}
+		if (this.align =="top" && this.position =="right") {
+			this.positionX = posRight
+			this.positionY = alignTop
+		}
+		if (this.align =="bottom" && this.position =="right") {
+			this.positionX = posRight
+			this.positionY = alignBottom
+		}
+
+		//Top
+		if (this.align =="center" && this.position =="top") {
+			this.positionX = alignCenterX
+			this.positionY = posTop
+		}
+		if (this.align =="right" && this.position =="top") {
+			this.positionX = alignRight
+			this.positionY = posTop
+		}
+		if (this.align =="left" && this.position =="top") {
+			this.positionX = alignLeft
+			this.positionY = posTop
+		}
+
+		//Left
+		if (this.align =="center" && this.position =="left") {
+			this.positionX = posLeft
+			this.positionY = alignCenterY
+		}
+		if (this.align =="top" && this.position =="left") {
+			this.positionX = posLeft
+			this.positionY = alignTop
+		}
+		if (this.align =="bottom" && this.position =="left") {
+			this.positionX = posLeft
+			this.positionY = alignBottom
+		}
+
+		//Apply style
+		this.popoverEl.style.transform = `translate3d(${this.positionX}px, ${this.positionY}px, 0px)`
+
+	}
+		
+
+	render() {
+    if (this.type =="static") {
+			return ([
+					
+				//Popover Toggle
+				<div class="has-popover">
+				<a href="#" class="popover-toggle"
+				onClick={() => this.open()}>Test</a>
+				</div>,
+				<div class={"popover" + (this.show ? " show" : "")} 
+				ref={(el: HTMLDivElement) => this.popoverEl = el}>
+				<slot name="content">
+					<div class="default__content">
+						<p>Your content goes here.</p>
+					</div>
+				</slot>
+				</div>
+				
+					
+			])  
+    } else { null }
+    if (this.type =="multi") {
+        return ([
+            <ul>
+							
+							<li class="has-popover">
+								<slot name="item2">
+									<a href="#" class="popover-toggle" 
+									onClick={() => this.open()}>POP 2</a>
+								</slot>
+								<div class={"popover" + (this.show ? " show" : "")} 
+								ref={(el: HTMLDivElement) => this.popoverEl = el}>
+								</div>
+							</li>
+						</ul>
+						
+
+        ])
+    } else { null }
+	}   
 }
