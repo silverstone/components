c6497d0366d6c0f314245c40068fd87484944f55
michaelauderer <mike@silverstone.io>
2018-10-08 21:49:12 -0400

    init storybook

---
+++ src/components/ss-button/ss-button.stories.js
@@ -0,0 +1,19 @@
+import { storiesOf } from '@storybook/html'
+import { withActions } from '@storybook/addon-actions'
+import { withKnobs, text, select } from '@storybook/addon-knobs'
+
+storiesOf('Button', module)
+  .addDecorator(withActions('click ss-button'))
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    const knobs = {
+      type: select('Type', ['basic', 'raised', 'flat', 'outline'], 'basic'),
+      color: select('Color', ['plain', 'primary', 'secondary', 'danger'], 'plain'),
+      text: text('Text', 'Click me'),
+    }
+    return `
+      <ss-button type="${knobs.type}" color="${knobs.color}">
+        ${knobs.text}
+      </ss-button>
+    `
+  })
