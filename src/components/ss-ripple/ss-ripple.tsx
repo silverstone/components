34caa617d4475d4eb113d30837d58ac58fe52229
Royce <royceshayegan@gmail.com>
2018-10-11 21:40:02 -0400

    remove comments

--- src/components/ss-ripple/ss-ripple.tsx
+++ src/components/ss-ripple/ss-ripple.tsx
@@ -21,35 +21,20 @@ export class RippleComponent {
     this.rippleFadeOut = false
     this.isRippleExpanded.emit(false)
     this.isRippleFadeOut.emit(false)
-    // console.log("component will load. ripple expanded is " +  this.rippleExpanded)
   }
 
   componentDidLoad() {
     this.el.style.transform = "scale(2)"
     this.el.addEventListener('transitionend', () => {
-      // this.rippleExpanded = true
-      // console.log("component did load. ripple expanded is " + this.rippleExpanded)
       this.isRippleExpanded.emit(true)
     }, true)
   }
 
   componentWillUpdate() {
-    // console.log("ripple will update")
-    // this.el.style.transition = "all 450ms ease-out"
-    // this.el.style.opacity = "0"
     this.el.addEventListener('transitionend', () => {
-      // this.rippleFadeOut = true
-      // console.log("ripple fadeout is " + this.rippleFadeOut)
       this.isRippleFadeOut.emit(true)
     })
   }
-
-  componentDidUpdate() {
-    // console.log("ripple updated")
-    // console.log("safe to delete node")
-    
-  }
-
 
 
   // render() {
